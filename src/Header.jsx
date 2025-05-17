2421537e3573669dcfbece8203865f73e3dcfb87
Sanskar Chaturvedi <ac77927@gmail.com>
2025-05-17 19:10:11 +0530

    temp

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,12 @@
 import React, { useState } from "react";
 import SideBar from "./SideBar";
-import { FaHamburger } from "react-icons/fa";
+import { FaHamburger, FaHome } from "react-icons/fa";
 import { GiHamburger, GiHamburgerMenu } from "react-icons/gi";
+import { GoMortarBoard } from "react-icons/go";
+import { BsFillMortarboardFill } from "react-icons/bs";
+import { GrGallery } from "react-icons/gr";
+import { IoCloudDownloadSharp, IoMegaphone } from "react-icons/io5";
+import { MdContactPhone } from "react-icons/md";
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -11,44 +16,83 @@ const Header = () => {
         <SideBar isopen={isOpen} setIsOpen={setIsOpen} />
       </div>
       <div className="flex">
-        <div className="min-h-32 h-24 sm:h-28  md:h-36 lg:h-40">
+        <div className="min-h-32 min-w-20 h-24 sm:h-28  md:h-36 lg:h-40 mx-2">
           <img
             src="/images/logo/logo.jpg"
             alt="error photo was not available yet"
-            className=" h-full  w-full object-contain "
+            className=" h-full  w-full object-contain"
           />
         </div>
-        <p className=" h-full w-full font-cancun flex text-3xl sm:text-5xl md:text-6xl md:flex-row flex-wrap gap-0 px-5 my-4 justify-center text-center">
-          <div>
-            <span className="text-primaryGreen p-2">CHILDREN'S</span>
-            <span className="text-primaryBlue p-2">VALLEY </span>
+
+        <div className="flex flex-col w-full px-2 my-4 gap-2">
+          <div className="h-full w-full font-cancun flex text-2xl sm:text-5xl md:text-6xl md:flex-row flex-wrap gap-x-5 gap-y-2 justify-center items-center text-center">
+            <div className="flex gap-2">
+              <span className="text-primaryGreen">CHILDREN'S</span>
+              <span className="text-primaryBlue ">VALLEY </span>
+            </div>
+            <div className="flex gap-2">
+              <span className="text-primaryRed">ENGLISH</span>
+              <span className="text-primaryPink">SCHOOL</span>
+            </div>
           </div>
-          <div>
-            <span className="text-primaryRed p-2">ENGLISH</span>
-            <span className="text-primaryPink p-2">SCHOOL</span>
-            <br />
+          <div className="w-full flex items-start justify-center">
+            <span className="text-sm md:text-xl lg:text-2xl font-serif font-semibold">
+              Mahmoorganj, Varanasi
+            </span>
           </div>
-          <span className="text-2xl font-serif font-semibold">
-            Mahmoorganj, Varanasi
-          </span>
-        </p>
+        </div>
       </div>
-      <div className="  h-10 bg-primaryGreen text-white ">
+      <div className="bg-primaryGreen text-white p-4">
         <div className="  text-2xl md:flex md:justify-center hidden sm:block">
           <ul className="flex gap-2 font-semibold">
-            <li className="cursor-pointer hover:text-black duration-500 ">
-              <a href="/">Home</a>
+            <li className="cursor-pointer hover:text-black duration-500 shadow shadow-white rounded-xl border border-white">
+              <a href="/" className="flex gap-2 items-center px-4 py-1">
+                <FaHome />
+                Home
+              </a>
             </li>
-            <li className="cursor-pointer hover:text-black duration-500 ">
-              About
+            <li className="cursor-pointer hover:text-black duration-500 shadow shadow-white rounded-xl border border-white">
+              <a
+                href="/sections=about_us"
+                className="flex gap-2 items-center px-4 py-1"
+              >
+                <IoMegaphone />
+                About Us
+              </a>
             </li>
-            <li>
-              <span className=" hover:text-black duration-500">
-                <a href="tel:9336576690">9336576690, </a>
-              </span>
-              <span className="hover:text-black duration-500">
-                <a href="tel:8081667099">8081667099</a>
-              </span>
+            <li className="cursor-pointer hover:text-black duration-500 shadow shadow-white rounded-xl border border-white">
+              <a
+                href="/sections=about_us"
+                className="flex gap-2 items-center px-4 py-1"
+              >
+                <BsFillMortarboardFill /> Affiliations
+              </a>
+            </li>
+            <li className="cursor-pointer hover:text-black duration-500 shadow shadow-white rounded-xl border border-white">
+              <a
+                href="/sections=photo_gallery"
+                className="flex gap-2 items-center px-4 py-1"
+              >
+                <GrGallery />
+                Photo Gallery
+              </a>
+            </li>
+            <li className="cursor-pointer hover:text-black duration-500 shadow shadow-white rounded-xl border border-white">
+              <a
+                href="/sections=downloads"
+                className="flex gap-2 items-center px-4 py-1"
+              >
+                <IoCloudDownloadSharp /> Downloads
+              </a>
+            </li>
+            <li className="cursor-pointer hover:text-black duration-500 shadow shadow-white rounded-xl border border-white">
+              <a
+                href="/sections=contact_us"
+                className="flex gap-2 items-center px-4 py-1"
+              >
+                <MdContactPhone />
+                Contact Us
+              </a>
             </li>
           </ul>
         </div>
