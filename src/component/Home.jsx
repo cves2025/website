d1b091734ae881dc57d2e296975bfb7aac3fccbc
martialrahulsharma <martialrahulsharma@gmail.com>
2025-11-16 16:12:05 +0530

    Refactor code structure for improved readability and maintainability

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import WelcomeToSchool from "./WelcomeToSchool.jsx";
 import NewsBox from "./NewsBox.jsx";
-import UpcomingEvent from "./UpcomingEvent.jsx";
+// import UpcomingEvent from "./UpcomingEvent.jsx";
 import Facilities from "./Facilities.jsx";
 
 function Home() {
@@ -10,7 +10,7 @@ function Home() {
       {/* <Banner /> */}
       <WelcomeToSchool />
       <NewsBox />
-      <UpcomingEvent />
+      {/* <UpcomingEvent /> */}
       <Facilities />
     </>
   );
